0473ab210429f08de9d4487427c9b738cd08f0be
17038568 <17038568@myrp.edu.sg>
2019-07-02 11:51:32 +0800

    Commit

--- app/src/main/java/sg/edu/rp/c346/demomyprofile/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/demomyprofile/MainActivity.java
@@ -4,11 +4,16 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText etName,etGPA;
+    RadioGroup rgGender;
+    Button btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,15 @@ public class MainActivity extends AppCompatActivity {
 
         etName = findViewById(R.id.editTextName);
         etGPA = findViewById(R.id.editTextGPA);
+        rgGender = findViewById(R.id.radioGroupGender);
+        btnSave = findViewById(R.id.buttonSave);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveData();
+            }
+        });
     }
 
     @Override
@@ -31,6 +45,13 @@ public class MainActivity extends AppCompatActivity {
 
         String Name = prefs.getString("name","");
         float GPA = prefs.getFloat("gpa",0);
+        String Gen = prefs.getString("gender","");
+
+        if(Gen.equals("Male")){
+            rgGender.check(R.id.radioButtonGenderMale);
+        }else{
+            rgGender.check(R.id.radioButtonGenderFemale);
+        }
         etName.setText(Name);
         etGPA.setText(GPA+"");
 
@@ -39,10 +60,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+        saveData();
+    }
 
+
+    public void saveData(){
         //step 1a get the user input from the EditText and store it in a variable
         String strName = etName.getText().toString();
         float gpa = Float.parseFloat(etGPA.getText().toString());
+        String gender = "";
+        if(rgGender.getCheckedRadioButtonId() == R.id.radioButtonGenderMale){
+            gender = "Male";
+        }else{
+            gender = "Female";
+        }
 
         //step 1b obtain an instance of the SharedPreferences
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
@@ -53,6 +84,7 @@ public class MainActivity extends AppCompatActivity {
         //step 1c add the key-value pair
         prefEdit.putString("name",strName);
         prefEdit.putFloat("gpa",gpa);
+        prefEdit.putString("gender",gender);
 
         //step 1d call commit() to save the changes into SharedPreferences
         prefEdit.commit();
